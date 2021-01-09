07248c96c310a8a2b9f7a3cfffcc84c7c20f9f56
Luis Rodriguez <ballestax@gmail.com>
2021-01-09 13:27:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 document.querySelector('.nav-menu-btn').addEventListener('click', () =>{
 	document.querySelector('.nav-menu').classList.toggle('show');
 });
+
+ScrollReveal().reveal('.carta');
+ScrollReveal().reveal('.carta__product', { delay: 500 });
